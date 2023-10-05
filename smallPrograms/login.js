95a9dbc5d300e710ca951a36e8ca49145a2703e1
arifoglu <88225121+arifoglu@users.noreply.github.com>
2023-10-05 14:59:13 +0200

    small programs finished

--- smallPrograms/login.js
+++ smallPrograms/login.js
@@ -1,15 +1,17 @@
 // my person's info arifoglu@gmail.com and password 12345
 let input1 = document.getElementById("email");
-console.log(input1);
+//console.log(input1);
 let input2 = document.getElementById("password");
-console.log(input2);
+//console.log(input2);
 
 function login(){
     if(input1.value == "arifoglu@gmail.com" && input2.value === "12345" )
     {
         alert ("login succesfully done")          
-    }else{
-        alert ("your infos are not true ,please try again")
+    }
+    else
+    {
+        alert ("please try again")
     }
     return 
 };
